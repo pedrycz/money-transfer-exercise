668e7674f0b119580f3e4bace1c66033d5fa6a78
ppedrycz <piotrekpedrycz@gmail.com>
2018-08-30 22:30:22 +0200

    Rest of tests

--- src/test/java/cz/pedry/moneytransfer/resource/TransferResourceTest.java
+++ src/test/java/cz/pedry/moneytransfer/resource/TransferResourceTest.java
@@ -1,14 +1,27 @@
 package cz.pedry.moneytransfer.resource;
 
+import cz.pedry.moneytransfer.model.Transfer;
 import cz.pedry.moneytransfer.service.TransferService;
 import io.dropwizard.testing.junit.ResourceTestRule;
 import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TransferResourceTest {
 
-    // ANALOGICAL TO AccountResourceTest
+    private static final long EXAMPLE_LONG = 1L;
+    private static final String VALID_TRANSFER = "{\"from\": {\"id\": 1}, \"to\": {\"id\": 2}, \"amount\": 500}";
+    private static final String INVALID_TRANSFER = "{\"from\": {\"id\": 1}, \"to\": {\"id\": 2}, \"amount\": 0}";
 
     private static final TransferService transferService = mock(TransferService.class);
 
@@ -17,11 +30,13 @@ public class TransferResourceTest {
             .addResource(new TransferResource(transferService))
             .build();
 
-    public void testSave() {
+    @Test
+    public void shouldCreateNewValidTransfer() {
+        doReturn(EXAMPLE_LONG).when(transferService).save(any(Transfer.class));
 
+        Response response = resources.client().target("/transfer").request().post(Entity.json(VALID_TRANSFER));
+
+        assertThat(response.readEntity(Long.class)).isEqualTo(EXAMPLE_LONG);
     }
 
-    public void testGet() {
-
-    }
 }
